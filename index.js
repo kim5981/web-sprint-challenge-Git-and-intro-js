8325b38c5ce31d47c43fd483a6b3b2450e57acba
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-01-14 10:42:19 -0800

    completed up to task 3, started task 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//this is an array called artists w/ 20 indexes; 8 dif keys for e/ artist
 const artists = [
   {
     "id": 0,
@@ -207,18 +208,22 @@ const artists = [
 Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
-//(1) Name of the first artist (0th index) in the array
+//(1) Name <-- of the first artist (0th index) in the array 
+//referencing array called artists
+
+console.log("- TASK 1A: ", artists[0].name);
 
 
-//(2) Bio of the third artist (2nd index) in the array 
+//(2) Bio <-- of the third artist (2nd index) in the array 
 
-
+console.log("- TASK 1B: ", artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
-
+artists[8].name = "Vincent Van Gogh";
+console.log("- TASK 2: ", artists[8].name);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -228,11 +233,11 @@ Use getArtistByIndex to do the following:
 
 Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, index) {
+  return `the artist at index ${index} is ${array[index].name}`;
 }
 
-
+console.log("- TASK 3:", getArtistByIndex(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use get20s to do the following: 
@@ -243,8 +248,11 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  const artistsOf20s = [];
+  for (let i = 0; i<array.length; i++){
+    if (array[array.includes(1900].years)
+  }
 }
 
 
